f22c5d007e23662249af16b37b84966268a05276
David Andersson <lejonmanen@gmail.com>
2020-05-14 10:59:26 +0200

    Added comments

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,7 +3,10 @@ import cat1 from '../img/cat1.jpg';
 import './gallery.css';
 
 const Gallery = () => {
+    // Use a state variable to control the layout
     const [columns, setColumns] = useState(2);
+
+    // Use a local variable for the exact value that the class property should have
     let columnsClass = 'gallery';
     if( columns === 1 ) {
         columnsClass += ' one-column';
@@ -19,7 +22,10 @@ const Gallery = () => {
                 <button onClick={() => setColumns(2)}>Two columns</button>
                 <button onClick={() => setColumns(3)}>Three columns</button>
             </div>
+
+            {/* convenient, but the image is included in the JS bundle, which make the JS larger and slower to load */}
             <img src={cat1} alt="Cat 1" />
+            {/* better, the images can be loaded asynchronously and be cached by the browser */}
             <img src="img/cat2.jpg" alt="Cat 2" />
             <img src="img/cat3.jpg" alt="Cat 3" />
             <img src="img/cat4.jpg" alt="Cat 4" />
