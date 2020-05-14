cb2a17915204b0d8edce0166fde86de0c4d92009
David Andersson <lejonmanen@gmail.com>
2020-05-14 11:48:54 +0200

    Inline styles and styled components

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,23 @@
 import React, { useState } from 'react';
 import cat1 from '../img/cat1.jpg';
 import './gallery.css';
+import styled from 'styled-components';
+
+
+const ButtonRow = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    transition: background-color 2s;
+    padding: 0.5em;
+
+    &:hover {
+        background-color: aqua;
+    }
+    & button {
+        margin: 0.5em;
+    }
+`;
+
 
 const Gallery = () => {
     // Use a state variable to control the layout
@@ -15,13 +32,22 @@ const Gallery = () => {
     } else if( columns === 3 ) {
         columnsClass += ' three-columns';
     }
+
+    const buttonStyle = {
+        fontWeight: 'bold'
+    };
+    const button1Style = columns===1 ? buttonStyle : null;
+
     return (
         <div className={columnsClass}>
-            <div>
-                <button onClick={() => setColumns(1)}>One column</button>
-                <button onClick={() => setColumns(2)}>Two columns</button>
-                <button onClick={() => setColumns(3)}>Three columns</button>
-            </div>
+            <ButtonRow>
+                <button onClick={() => setColumns(1)}
+                    style={button1Style}>One column</button>
+                <button onClick={() => setColumns(2)}
+                    style={columns===2 ? buttonStyle : null}>Two columns</button>
+                <button onClick={() => setColumns(3)}
+                    style={columns===3 ? buttonStyle : null}>Three columns</button>
+            </ButtonRow>
 
             {/* convenient, but the image is included in the JS bundle, which make the JS larger and slower to load */}
             <img src={cat1} alt="Cat 1" />
